1e880e4617fd67ee9a4df64748adf9f52743804e
ngk <nehuenian@gmail.com>
2021-06-21 09:57:15 -0300

    Prevent the use of dark mode to avoid bad UI. Change the position of the first restaurant list refresh to the init of the view model, so it won't load to any configuration change

--- app/src/main/java/com/takeaway/assignment/AssignmentApplication.kt
+++ app/src/main/java/com/takeaway/assignment/AssignmentApplication.kt
@@ -1,7 +1,13 @@
 package com.takeaway.assignment
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class AssignmentApplication : Application()
+class AssignmentApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
+}
